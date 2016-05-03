d6ced56b39344bc5c089164b98bf3fd709540870
James W <jameswalsh5683@gmail.com>
2016-05-03 23:22:58 +0100

    Initial Commit

---
+++ PlugController/FrmMain.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PlugController
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.Btn1ON = new System.Windows.Forms.Button();
+            this.Btn1OFF = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.Btn2OFF = new System.Windows.Forms.Button();
+            this.Btn2ON = new System.Windows.Forms.Button();
+            this.Btn4OFF = new System.Windows.Forms.Button();
+            this.Btn4ON = new System.Windows.Forms.Button();
+            this.Btn3OFF = new System.Windows.Forms.Button();
+            this.Btn3ON = new System.Windows.Forms.Button();
+            this.BtnALLOFF = new System.Windows.Forms.Button();
+            this.BtnALLON = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn1ON
+            // 
+            this.Btn1ON.Location = new System.Drawing.Point(13, 13);
+            this.Btn1ON.Name = "Btn1ON";
+            this.Btn1ON.Size = new System.Drawing.Size(75, 23);
+            this.Btn1ON.TabIndex = 0;
+            this.Btn1ON.Text = "1 ON";
+            this.Btn1ON.UseVisualStyleBackColor = true;
+            this.Btn1ON.Click += new System.EventHandler(this.Btn1ON_Click);
+            // 
+            // Btn1OFF
+            // 
+            this.Btn1OFF.Location = new System.Drawing.Point(100, 13);
+            this.Btn1OFF.Name = "Btn1OFF";
+            this.Btn1OFF.Size = new System.Drawing.Size(75, 23);
+            this.Btn1OFF.TabIndex = 4;
+            this.Btn1OFF.Text = "1 OFF";
+            this.Btn1OFF.UseVisualStyleBackColor = true;
+            this.Btn1OFF.Click += new System.EventHandler(this.Btn1OFF_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(204, 158);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(20, 20);
+            this.webBrowser1.TabIndex = 10;
+            // 
+            // Btn2OFF
+            // 
+            this.Btn2OFF.Location = new System.Drawing.Point(100, 42);
+            this.Btn2OFF.Name = "Btn2OFF";
+            this.Btn2OFF.Size = new System.Drawing.Size(75, 23);
+            this.Btn2OFF.TabIndex = 12;
+            this.Btn2OFF.Text = "2 OFF";
+            this.Btn2OFF.UseVisualStyleBackColor = true;
+            this.Btn2OFF.Click += new System.EventHandler(this.Btn2OFF_Click);
+            // 
+            // Btn2ON
+            // 
+            this.Btn2ON.Location = new System.Drawing.Point(13, 42);
+            this.Btn2ON.Name = "Btn2ON";
+            this.Btn2ON.Size = new System.Drawing.Size(75, 23);
+            this.Btn2ON.TabIndex = 11;
+            this.Btn2ON.Text = "2 ON";
+            this.Btn2ON.UseVisualStyleBackColor = true;
+            this.Btn2ON.Click += new System.EventHandler(this.Btn2ON_Click);
+            // 
+            // Btn4OFF
+            // 
+            this.Btn4OFF.Enabled = false;
+            this.Btn4OFF.Location = new System.Drawing.Point(100, 100);
+            this.Btn4OFF.Name = "Btn4OFF";
+            this.Btn4OFF.Size = new System.Drawing.Size(75, 23);
+            this.Btn4OFF.TabIndex = 16;
+            this.Btn4OFF.Text = "4 OFF";
+            this.Btn4OFF.UseVisualStyleBackColor = true;
+            this.Btn4OFF.Click += new System.EventHandler(this.Btn4OFF_Click);
+            // 
+            // Btn4ON
+            // 
+            this.Btn4ON.Enabled = false;
+            this.Btn4ON.Location = new System.Drawing.Point(13, 100);
+            this.Btn4ON.Name = "Btn4ON";
+            this.Btn4ON.Size = new System.Drawing.Size(75, 23);
+            this.Btn4ON.TabIndex = 15;
+            this.Btn4ON.Text = "4 ON";
+            this.Btn4ON.UseVisualStyleBackColor = true;
+            this.Btn4ON.Click += new System.EventHandler(this.Btn4ON_Click);
+            // 
+            // Btn3OFF
+            // 
+            this.Btn3OFF.Location = new System.Drawing.Point(100, 71);
+            this.Btn3OFF.Name = "Btn3OFF";
+            this.Btn3OFF.Size = new System.Drawing.Size(75, 23);
+            this.Btn3OFF.TabIndex = 14;
+            this.Btn3OFF.Text = "3 OFF";
+            this.Btn3OFF.UseVisualStyleBackColor = true;
+            this.Btn3OFF.Click += new System.EventHandler(this.Btn3OFF_Click);
+            // 
+            // Btn3ON
+            // 
+            this.Btn3ON.Location = new System.Drawing.Point(13, 71);
+            this.Btn3ON.Name = "Btn3ON";
+            this.Btn3ON.Size = new System.Drawing.Size(75, 23);
+            this.Btn3ON.TabIndex = 13;
+            this.Btn3ON.Text = "3 ON";
+            this.Btn3ON.UseVisualStyleBackColor = true;
+            this.Btn3ON.Click += new System.EventHandler(this.Btn3ON_Click);
+            // 
+            // BtnALLOFF
+            // 
+            this.BtnALLOFF.Location = new System.Drawing.Point(100, 129);
+            this.BtnALLOFF.Name = "BtnALLOFF";
+            this.BtnALLOFF.Size = new System.Drawing.Size(75, 23);
+            this.BtnALLOFF.TabIndex = 18;
+            this.BtnALLOFF.Text = "ALL OFF";
+            this.BtnALLOFF.UseVisualStyleBackColor = true;
+            this.BtnALLOFF.Click += new System.EventHandler(this.BtnALLOFF_Click);
+            // 
+            // BtnALLON
+            // 
+            this.BtnALLON.Location = new System.Drawing.Point(13, 129);
+            this.BtnALLON.Name = "BtnALLON";
+            this.BtnALLON.Size = new System.Drawing.Size(75, 23);
+            this.BtnALLON.TabIndex = 17;
+            this.BtnALLON.Text = "ALL ON";
+            this.BtnALLON.UseVisualStyleBackColor = true;
+            this.BtnALLON.Click += new System.EventHandler(this.BtnALLON_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 171);
+            this.Controls.Add(this.BtnALLOFF);
+            this.Controls.Add(this.BtnALLON);
+            this.Controls.Add(this.Btn4OFF);
+            this.Controls.Add(this.Btn4ON);
+            this.Controls.Add(this.Btn3OFF);
+            this.Controls.Add(this.Btn3ON);
+            this.Controls.Add(this.Btn2OFF);
+            this.Controls.Add(this.Btn2ON);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.Btn1OFF);
+            this.Controls.Add(this.Btn1ON);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Remote Plug Controller";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1ON;
+        private System.Windows.Forms.Button Btn1OFF;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button Btn2OFF;
+        private System.Windows.Forms.Button Btn2ON;
+        private System.Windows.Forms.Button Btn4OFF;
+        private System.Windows.Forms.Button Btn4ON;
+        private System.Windows.Forms.Button Btn3OFF;
+        private System.Windows.Forms.Button Btn3ON;
+        private System.Windows.Forms.Button BtnALLOFF;
+        private System.Windows.Forms.Button BtnALLON;
+    }
+}
+
---
+++ PlugController/FrmMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PlugController
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1ON_Click(object sender, EventArgs e)
+        {
+            TurnLightOn(1);
+        }
+
+        private void Btn1OFF_Click(object sender, EventArgs e)
+        {
+            TurnLightOff(1);
+
+        }
+
+        private void Btn2ON_Click(object sender, EventArgs e)
+        {
+            TurnLightOn(2);
+
+        }
+
+        private void Btn2OFF_Click(object sender, EventArgs e)
+        {
+            TurnLightOff(2);
+
+        }
+
+        private void Btn3ON_Click(object sender, EventArgs e)
+        {
+            TurnLightOn(3);
+
+        }
+
+        private void Btn3OFF_Click(object sender, EventArgs e)
+        {
+            TurnLightOff(3);
+
+        }
+
+        private void Btn4ON_Click(object sender, EventArgs e)
+        {
+            TurnLightOn(4);
+
+        }
+
+        private void Btn4OFF_Click(object sender, EventArgs e)
+        {
+            TurnLightOff(4);
+
+        }
+
+        private void BtnALLON_Click(object sender, EventArgs e)
+        {
+            TurnLightOn(0);
+
+        }
+
+        private void BtnALLOFF_Click(object sender, EventArgs e)
+        {
+            TurnLightOff(0);
+
+        }
+
+        private void TurnLightOn(int LightNo)
+        {
+            if (LightNo == 0)
+            {
+                webBrowser1.Navigate("http://192.168.0.15/control.php?allon");
+
+            }
+            else
+            {
+                webBrowser1.Navigate("http://192.168.0.15/control.php?" + LightNo + "on");
+
+            }
+        }
+
+        private void TurnLightOff(int LightNo)
+        {
+            if (LightNo == 0)
+            {
+                webBrowser1.Navigate("http://192.168.0.15/control.php?alloff");
+
+            }
+            else
+            {
+                webBrowser1.Navigate("http://192.168.0.15/control.php?" + LightNo + "off");
+
+            }
+        }
+
+    }
+}
